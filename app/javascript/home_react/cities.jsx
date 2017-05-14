3fabf5d456dedd608eb8d12e59ecffdbebaf7d40
Earl Bunao <earle.bunao@gmail.com>
2017-05-14 20:00:00 +0800

    Use flex as grid

--- app/javascript/home_react/cities.jsx
+++ app/javascript/home_react/cities.jsx
@@ -3,7 +3,7 @@ import City from './city';
 
 function Cities({ weather_details }) {
   return (
-    <div className="columns columns-endless">
+    <div className="card-grid">
       {weather_details.map((weather_detail, i) => {
         return <City key={i} weather_detail={weather_detail}/>;
       })}
--- app/javascript/home_react/city.jsx
+++ app/javascript/home_react/city.jsx
@@ -4,32 +4,30 @@ import StringUtils from './string_utils';
 
 function City({ weather_detail }) {
   return (
-    <div className="column-4">
-      <div className="card">
-        <div>
-          <h2 className="card__subheader">
-            {weather_detail.name}
-          </h2>
-          <p className="card__content">
-            {moment().format("MMM Do")}
-          </p>
-        </div>
-        <div className="card__main">
-          <h2 className="card__header">
-            {Math.round(weather_detail.main.temp)}
-          </h2>
-          <p className="card__content">
-            {StringUtils.capitalize(weather_detail.weather[0].description)}
-          </p>
-        </div>
-        <div className="card__details">
-          <p className="card__content">
-            Humidity: {`${weather_detail.main.humidity}%`}
-          </p>
-          <p className="card__content">
-            Wind: {`${weather_detail.wind.speed}km/h`}
-          </p>
-        </div>
+    <div className="card">
+      <div className="card__details">
+        <h2 className="card__subheader">
+          {weather_detail.name}
+        </h2>
+        <p className="card__content">
+          {moment().format("MMM Do")}
+        </p>
+      </div>
+      <div className="card__main">
+        <h2 className="card__header">
+          {Math.round(weather_detail.main.temp)}
+        </h2>
+        <p className="card__content">
+          {StringUtils.capitalize(weather_detail.weather[0].description)}
+        </p>
+      </div>
+      <div className="card__subdetails">
+        <p className="card__content">
+          Humidity: {`${weather_detail.main.humidity}%`}
+        </p>
+        <p className="card__content">
+          Wind: {`${weather_detail.wind.speed}km/h`}
+        </p>
       </div>
     </div>
   );
